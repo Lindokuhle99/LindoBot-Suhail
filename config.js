7a9f5487c8ea5934afa2a357408719f7af1af941
Lindokuhle King <164007390+Lindokuhle99@users.noreply.github.com>
2024-08-01 14:02:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -84,7 +84,7 @@ module.exports = {
   KOYEB_API : process.env.KOYEB_API  || "false",
 
   REMOVE_BG_KEY : process.env.REMOVE_BG_KEY  || "",
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "sk-None-zEehZ7V8BjhHVEEH871FT3BlbkFJjZp1aXEsgy2683Pb3VIy",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   HEROKU_APP_NAME:process.env.HEROKU_APP_NAME|| "",
   antilink_values:process.env.ANTILINK_VALUES|| "all",
